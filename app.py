726bc461721e874e9feb3835cbd8dc05f16678ef
Renan Rosental <renan.engal@gmail.com>
2024-06-24 20:16:17 -0300

    reseting data

--- app.py
+++ app.py
@@ -1,41 +1,34 @@
-import streamlit as st
 import pandas as pd
+import plotly.express as px
+import streamlit as st
 
-# Lendo os dados de um arquivo CSV
+car_data = pd.read_csv('vehicles_us.csv')  # lendo os dados
 
+st.header(":sunglasses: Renan's First App Web :car:", divider='rainbow')
 
-@st.cache
-def load_data():
-    data = pd.read_csv('vehicles_us.csv')
-    return data
+# criar uma caixa de seleção para gerar um histograma
+build_hist = st.checkbox('Criar histograma')
 
+if build_hist:  # se a caixa de seleção for selecionada
+    # escrever uma mensagem
+    st.write(
+        'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
 
-data = load_data()
+    # criar um histograma
+    fig_hist = px.histogram(car_data, x="odometer")
 
-st.title('Análise de Veículos')
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig_hist, use_container_width=True)
 
-# Filtro de seleção múltipla para a marca do veículo
-marcas = st.multiselect('Escolha a(s) marca(s)', options=data['manufacturer'].unique(
-), default=data['manufacturer'].unique())
+# criar uma caixa de seleção para gerar um gráfico de dispersão
+build_scatter = st.checkbox('Criar gráfico de dispersão')
 
-# Filtro de intervalo para o ano do veículo
-ano_min, ano_max = st.slider('Escolha o intervalo de anos', int(data['year'].min(
-)), int(data['year'].max()), (int(data['year'].min()), int(data['year'].max())))
+if build_scatter:  # se a caixa de seleção for selecionada
+    # escrever uma mensagem
+    st.write(
+        'Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
 
-# Filtro de faixa de preço
-preco_min, preco_max = st.slider('Escolha o intervalo de preços', int(data['price'].min(
-)), int(data['price'].max()), (int(data['price'].min()), int(data['price'].max())))
-
-# Aplicando os filtros aos dados
-data_filtrada = data[
-    (data['manufacturer'].isin(marcas)) &
-    (data['year'] >= ano_min) & (data['year'] <= ano_max) &
-    (data['price'] >= preco_min) & (data['price'] <= preco_max)
-]
-
-st.write(f'Dados Filtrados ({data_filtrada.shape[0]} linhas)')
-st.dataframe(data_filtrada)
-
-# Exibindo um gráfico de dispersão dos dados filtrados
-st.write('Gráfico de dispersão do odômetro vs. preço')
-st.plotly_chart(px.scatter(data_filtrada, x='odometer', y='price'))
+    # criar um histograma
+    fig_scatter = px.scatter(car_data, x="odometer", y="price")
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig_scatter, use_container_width=True)
