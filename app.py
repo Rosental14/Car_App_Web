3871eb9353df709cc9073a2bf8d1128a61f1b688
Renan Rosental <renan.engal@gmail.com>
2024-06-24 20:45:49 -0300

    teste 1

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ data = load_data()
 st.title('Análise de Veículos')
 
 # Filtro de seleção múltipla para a marca do veículo
-marcas = st.multiselect('Escolha a(s) marca(s)', options=data['manufacturer'].unique(), default=data['manufacturer'].unique())
+modelo = st.multiselect('Escolha o(s) modelos(s)', options=data['model'].unique(), default=data['model'].unique())
 
 # Filtro de intervalo para o ano do veículo
 ano_min, ano_max = st.slider('Escolha o intervalo de anos', int(data['year'].min()), int(data['year'].max()), (int(data['year'].min()), int(data['year'].max())))
@@ -23,8 +23,8 @@ preco_min, preco_max = st.slider('Escolha o intervalo de preços', int(data['pri
 
 # Aplicando os filtros aos dados
 data_filtrada = data[
-    (data['manufacturer'].isin(marcas)) &
-    (data['year'] >= ano_min) & (data['year'] <= ano_max) &
+    (data['model'].isin(marcas)) &
+    (data['model_year'] >= ano_min) & (data['year'] <= ano_max) &
     (data['price'] >= preco_min) & (data['price'] <= preco_max)
 ]
 
