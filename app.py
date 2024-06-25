b9227c0442a99024088e43387a2b3435e066eba8
Renan Rosental <renan.engal@gmail.com>
2024-06-24 23:27:23 -0300

    teste 5

--- app.py
+++ app.py
@@ -43,25 +43,10 @@ build_hist = st.checkbox('Criar histograma')
 
 if build_hist:  # se a caixa de seleção for selecionada
     # escrever uma mensagem
-    st.write(
-        'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
 
     # criar um histograma
-    fig_hist = px.histogram(data, x="odometer")
+    fig_hist = px.histogram(data_filtrada, x="odometer")
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig_hist, use_container_width=True)
-    
-    
-    # criar uma caixa de seleção para gerar um gráfico de dispersão
-build_scatter = st.checkbox('Criar gráfico de dispersão')
-
-if build_scatter:  # se a caixa de seleção for selecionada
-    # escrever uma mensagem
-    st.write(
-        'Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
-
-    # criar um histograma
-    fig_scatter = px.scatter(data, x="odometer", y="price")
-    # exibir um gráfico Plotly interativo
-    st.plotly_chart(fig_scatter, use_container_width=True)
