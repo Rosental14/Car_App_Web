bc91103a92c5509889cf7cb6a756b5b735980608
Renan Rosental <renan.engal@gmail.com>
2024-06-24 23:17:15 -0300

    teste 4

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ def load_data():
 
 data = load_data()
 
+st.header(":sunglasses: Renan's First App Web :car:", divider='rainbow')
+
 st.title('Análise de Veículos')
 
 # Filtro de seleção múltipla para a marca do veículo
@@ -34,3 +36,32 @@ st.dataframe(data_filtrada)
 # Exibindo um gráfico de dispersão dos dados filtrados
 st.write('Gráfico de dispersão do odômetro vs. preço')
 st.plotly_chart(px.scatter(data_filtrada, x='odometer', y='price'))
+
+
+# criar uma caixa de seleção para gerar um histograma
+build_hist = st.checkbox('Criar histograma')
+
+if build_hist:  # se a caixa de seleção for selecionada
+    # escrever uma mensagem
+    st.write(
+        'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+
+    # criar um histograma
+    fig_hist = px.histogram(data, x="odometer")
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig_hist, use_container_width=True)
+    
+    
+    # criar uma caixa de seleção para gerar um gráfico de dispersão
+build_scatter = st.checkbox('Criar gráfico de dispersão')
+
+if build_scatter:  # se a caixa de seleção for selecionada
+    # escrever uma mensagem
+    st.write(
+        'Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+
+    # criar um histograma
+    fig_scatter = px.scatter(data, x="odometer", y="price")
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig_scatter, use_container_width=True)
